9e23a04dd3f27f6e6d3ffffc62d58b88e9c112dc
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-07-14 15:08:20 -0300

    Incluindo o banco de dados

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE BDEscola
+USE BDEscola
+
+CREATE TABLE Escola
+(
+IDEscola INT PRIMARY KEY,
+Nome VARCHAR (100),
+Endereco VARCHAR (100)
+);
+
+CREATE TABLE Professor
+(
+IDProfessor INT PRIMARY KEY,
+Nome VARCHAR (100),
+Registro int
+);
+
+
