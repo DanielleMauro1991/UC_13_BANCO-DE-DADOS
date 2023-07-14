fbc5214d4ca98101057f49aca576e5a7f4e9fdc8
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-07-14 15:24:49 -0300

    Criando tabelas

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,9 +10,43 @@ Endereco VARCHAR (100)
 
 CREATE TABLE Professor
 (
-IDProfessor INT PRIMARY KEY,
+Professor_ID INT PRIMARY KEY,
 Nome VARCHAR (100),
 Registro int
 );
 
+create table Escola_Professor
 
+(
+IDEscola INT foreign KEY references Escola(IDEscola),
+Professor_ID INT foreign KEY references Professor(Professor_ID)
+
+);
+
+CREATE TABLE Materia
+(
+IDMateria INT PRIMARY KEY,
+Nome VARCHAR (100),
+Descricao varchar (100)
+);
+
+CREATE TABLE Professor_Materia
+(
+Professor_ID INT foreign KEY references Professor(Professor_ID),
+IDMateria INT foreign KEY references Materia(IDMateria)
+);
+
+
+CREATE TABLE Aluno
+(
+IDAluno INT PRIMARY KEY,
+IDEscola INT foreign KEY references Escola(IDEscola),
+Nome VARCHAR (100),
+RA VARCHAR (100)
+);
+
+CREATE TABLE Aluno_Materia
+(
+IDAluno INT foreign KEY references Aluno(IDAluno),
+IDMateria INT foreign KEY references Materia(IDMateria)
+);
